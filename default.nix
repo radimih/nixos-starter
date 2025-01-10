249248755d1f4654e1f3b76e76884c0b75040aa3
Radimir Mikhailov <radimir.com@gmail.com>
2025-01-10 09:27:08 +0000

    Упрощён default.nix

--- default.nix
+++ default.nix
@@ -1,9 +1,5 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
-in
-
-pkgs.mkShellNoCC {
+with (import <nixpkgs> {});
+mkShellNoCC {
   packages = with pkgs; [
     cowsay
     lolcat
